df95d94e9d8d8d405dfa9d1e2f0c87cfc805d3de
nickdibartolo1 <dibartolonick0518@gmail.com>
2022-08-27 12:43:37 -0400

    added manager object with input data

--- index.js
+++ index.js
@@ -1,2 +1,63 @@
-const fs = require('fs'); 
+const fs = require('fs');
 const inquirer = require('inquirer');
+
+
+
+const workerArr = [];
+
+const addManager = () => {
+    return inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            question: "What is the team managers name?:",
+            validate: nameEntry => {
+                if (nameEntry) {
+                    return true;
+                } else {
+                    console.log("A name is needed for this entry")
+                    return false
+                }
+            }
+
+        },
+        {
+            name: "id",
+            type: "input",
+            question: "What is the team managers ID number?:",
+            validate: nameEntry => {
+                if (nameEntry === isNaN) {
+                    console.log("Enter the team managers ID number.")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+        },
+        {
+            name: "email",
+            type: "input",
+            question: "What is the team managers email address?:",
+            // validate: nameEntry => {
+            //     if (nameEntry === email){
+
+            //     }
+            // }
+        },
+        {
+            name: "officeNumber",
+            type: "input",
+            question: "What is the team managers office number?:",
+            validate: nameEntry => {
+                if (nameEntry === isNaN) {
+                    console.log("Enter the team managers office number.")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+    ])
+}

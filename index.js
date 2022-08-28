4f735959babe8a64e5b244c73297c7b31ad3b3ba
nickdibartolo1 <dibartolonick0518@gmail.com>
2022-08-28 11:14:53 -0400

    added employee object data, added then statement for pushing object data into the array for manager object

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const inquirer = require('inquirer');
 
 const workerArr = [];
 
+//managers object info
 const addManager = () => {
     return inquirer.prompt([
         {
@@ -59,5 +60,75 @@ const addManager = () => {
             }
         }
 
+    ])
+    .then(managerEntry => {
+        const {name, id, email, officeNumber} = managerEntry;
+        const addedManager = new addedManager (name, id, email, officeNumber);
+        workerArr.push(manager);
+        console.log(manager);
+    })
+};
+
+
+//employees object info
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+            name: "job",
+            type: "list",
+            question: "What is the job of the employee?:",
+            options: ["Engineer", "Intern"]
+        },
+        {
+            name: "name",
+            type: "input",
+            question: "What is the employees name?:",
+            validate: nameEntry => {
+                if (nameEntry) {
+                    return true;
+                } else {
+                    console.log("A name is needed for this entry")
+                    return false
+                }
+            }
+
+        },
+        {
+            name: "id",
+            type: "input",
+            question: "What is the team managers ID number?:",
+            validate: nameEntry => {
+                if (nameEntry === isNaN) {
+                    console.log("Enter the team managers ID number.")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+        },
+        {
+            name: "id",
+            type: "input",
+            question: "What is the employees ID number?:",
+            validate: nameEntry => {
+                if (nameEntry === isNaN) {
+                    console.log("Enter the employees ID number.")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+        },
+        {
+            name: "email",
+            type: "input",
+            question: "What is the employees email address?:",
+            //
+        },
+        {
+
+        }
     ])
 }
